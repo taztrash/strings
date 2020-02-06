ed2eb57ee68f8db3c976308a5e4a9c8bc472945f
taztrash <teddymwangi8834@gmail.com>
2020-02-06 09:54:50 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,46 @@
+# string indexing and slicing
+a="hello world!"
+print (a)
+#each 
+print (a[0])
+print (a[1])
+print (a[2])
+print (a[3])
+print (a[4])
+print (a[5])
+print (a[6])
+print (a[7])
+print (a[8])
+print (a[9])
+print (a[10])
+print (a[11])
+print (40)
+print (a[2:5])
+print (a[2:9])
+print (a[:])
+
+
+
+#string operators
+a="Hello"
+b="world"
+print(a+b)
+print (a*4)
+print (r"C:\Users\Teddy\Documents\python")
+
+
+
+
+
+#built-in string function/methods
+n="HEllO"
+a= "Hello, World"
+b =a.capitalize()#it capitalizes the first letter
+print (b)
+print(a.capitalize())
+print(a.upper())#changes all lower case strings to upper case
+print(n.lower())#changes all upper case strings to lower case
+print(a.casefold())#changes all upper case strings to lower case
+print(a.strip())#removes any whitespace from the beginning or the end
+print(a.replace("H", "J"))#replaces a string with another string
+print(a.split(","))#splits the string into substrings if it finds instances of the separator
